35ae02c240b2de198c33e42e20a0cece6c5000ca
ocelotdive <oscar1dlh@hotmail.com>
2019-02-25 12:18:40 +0000

    removed unused finish panel classes

--- src/components/FinishPanel.js
+++ src/components/FinishPanel.js
@@ -4,45 +4,36 @@ import { connect } from 'react-redux';
 
 
 class FinishPanel extends Component {
-	
 
-	
-	render() {
-		let onScreen = ['lightSpeedIn'];
-		let offScreen = ['lightSpeedOut'];
-		
-		
-		
-	let	finishClass = this.props.finishShow ? 'finishPanel animated ' + onScreen[0] : 'finishPanel animated ' + offScreen[0];
-		
-		return (
-		this.props.finishShow ?
-		<section className={finishClass} ref="finishPanel">
-			<h2>COMPLETED!</h2>
-			<h3>WELL DONE</h3>
-			<div className="circle"></div>
-		
-		</section>
-		:
-			<section>
-			</section>
-		)
-	}
+// removed class names for finish panel
+
+    render() {
+
+  
+
+        return (
+
+            this.props.finishShow ?
+
+            <section className ='finishPanel animated lightSpeedIn'>
+            
+                <h2> COMPLETED! </h2> 
+                <h3> WELL DONE </h3> 
+                <div className = "circle">< /div> 
+            
+            </section> :
+            
+            <section>
+            </section>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		finishShow: state.finishShow
-	
-	}
+    return {
+        finishShow: state.finishShow
+    }
 }
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-        startLesson: (lessonNumber,keys) => {dispatch({type: 'STARTLESSON', lessonNumber: lessonNumber, keys: keys})},
-		
-	
-	}
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(FinishPanel);
+export default connect(mapStateToProps)(FinishPanel);
